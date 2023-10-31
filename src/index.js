7c1e53f16a14df542c953b986150205aa0dbfe2f
nguyenquocviet1002 <vietnguyen10022001@gmail.com>
2023-10-31 08:33:19 +0700

    update

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+      <BrowserRouter basename="/app/seeding-v2">
         <App />
       </BrowserRouter>
     </QueryClientProvider>

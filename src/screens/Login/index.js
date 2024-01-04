e68eceeb7be429503d74f993a35ddf3cd9c79f11
nguyenquocviet1002 <vietnguyen10022001@gmail.com>
2024-01-04 08:27:17 +0700

    update login

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -3,6 +3,7 @@ import { useNavigate, Navigate } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import { useLocalStorage } from '@/hooks/useLocalStorage';
 import { getTokenFn } from '@/api/auth';
+import Loading from '@/components/Loading';
 import loginStyles from './Login.module.scss';
 
 const ScreenLogin = () => {
@@ -14,12 +15,15 @@ const ScreenLogin = () => {
   const [token, setToken] = useLocalStorage('token-manage', null);
   const [user, setUser] = useState(initialUser);
   const [message, setMessage] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [isTypeP, setIsTypeP] = useState(false);
 
   const queryLogin = useQuery({
     queryKey: ['login', user.user],
     queryFn: () => getTokenFn(user),
     enabled: false,
     onSuccess: (data) => {
+      setIsLoading(false);
       if (data.data.message && data.data.type === 'missing error') {
         setMessage('Tài khoản hoặc mật khẩu không được để trống');
       } else if (data.data.message && data.data.type === 'Access denied') {
@@ -38,8 +42,10 @@ const ScreenLogin = () => {
     setUser((prev) => ({ ...prev, [name]: event.target.value }));
   };
   const handleSubmit = () => {
+    setIsLoading(true);
     if (!user.user || !user.password) {
       setMessage('Số điện thoại và mật khẩu không được để trống');
+      setIsLoading(false);
     } else {
       queryLogin.refetch();
     }
@@ -49,40 +55,54 @@ const ScreenLogin = () => {
   }
 
   return (
-    <div className={loginStyles['login']}>
-      <div className={loginStyles['login__box']}>
-        <div className={loginStyles['login__right']}>
-          <img width={180} height={95} src={`${process.env.PUBLIC_URL}/images/logo.svg`} alt="" />
-        </div>
-        <div className={loginStyles['login__left']}>
-          <div className={loginStyles['login__title']}>Quản trị</div>
-          {message && <p className={loginStyles['login__message']}>{message}</p>}
-          <div className={loginStyles['login__group']}>
-            <label>Số điện thoại</label>
-            <input
-              type="text"
-              className={message ? loginStyles['login__error'] : ''}
-              value={user.user}
-              onChange={handleChange('user')}
-              onKeyDown={(e) => (e.key === 'Enter' ? handleSubmit() : '')}
-            />
+    <>
+      <div className={loginStyles['login']}>
+        <div className={loginStyles['login__box']}>
+          <div className={loginStyles['login__right']}>
+            <img width={180} height={95} src={`${process.env.PUBLIC_URL}/images/logo.svg`} alt="" />
           </div>
-          <div className={loginStyles['login__group']}>
-            <label>Mật khẩu</label>
-            <input
-              type="password"
-              className={message ? loginStyles['login__error'] : ''}
-              value={user.password}
-              onChange={handleChange('password')}
-              onKeyDown={(e) => (e.key === 'Enter' ? handleSubmit() : '')}
-            />
+          <div className={loginStyles['login__left']}>
+            <div className={loginStyles['login__title']}>Quản trị</div>
+            {message && <p className={loginStyles['login__message']}>{message}</p>}
+            <div className={loginStyles['login__group']}>
+              <label>Số điện thoại</label>
+              <input
+                type="text"
+                className={message ? loginStyles['login__error'] : ''}
+                value={user.user}
+                onChange={handleChange('user')}
+                onKeyDown={(e) => (e.key === 'Enter' ? handleSubmit() : '')}
+              />
+            </div>
+            <div className={loginStyles['login__group']}>
+              <label>Mật khẩu</label>
+              <div className={loginStyles['login__control']}>
+                <input
+                  type={!isTypeP ? 'password' : 'text'}
+                  className={message ? loginStyles['login__error'] : ''}
+                  value={user.password}
+                  onChange={handleChange('password')}
+                  onKeyDown={(e) => (e.key === 'Enter' ? handleSubmit() : '')}
+                />
+                <span
+                  className={loginStyles['login__change']}
+                  style={{
+                    backgroundImage: !isTypeP
+                      ? `url(${process.env.PUBLIC_URL}/images/eye-regular.svg)`
+                      : `url(${process.env.PUBLIC_URL}/images/eye-slash-regular.svg)`,
+                  }}
+                  onClick={() => setIsTypeP(!isTypeP)}
+                ></span>
+              </div>
+            </div>
+            <button className={loginStyles['login__btn']} onClick={() => handleSubmit()}>
+              Đăng nhập
+            </button>
           </div>
-          <button className={loginStyles['login__btn']} onClick={() => handleSubmit()}>
-            Đăng nhập
-          </button>
         </div>
       </div>
-    </div>
+      {isLoading && <Loading />}
+    </>
   );
 };
 
